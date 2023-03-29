f128f33d8284f081e046208780945926bc48c01b
takuya-builcop <124562101+takuya-builcop@users.noreply.github.com>
2023-03-29 22:15:27 +0900

    Create survey_page.dart

---
+++ survey_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'survey_page_state.dart';
+
+class SurveyPage extends StatefulWidget {
+  final String imagePath;
+  final String imageId;
+  final String imageName;
+  final String surveyId;
+  final String drawingId;
+
+  SurveyPage({
+    required this.imagePath,
+    required this.imageId,
+    required this.imageName,
+    required this.surveyId,
+    required this.drawingId,
+  });
+
+  @override
+  SurveyPageState createState() => SurveyPageState();
+}
